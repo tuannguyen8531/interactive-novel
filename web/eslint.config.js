ae46224d496c6533e7f7aa51086f7697ebcfb674
SeventhS <tuannguyen8531@gmail.com>
2026-08-31 09:34:57 +0700

    fix(eslint): update export to use defineConfig for better configuration

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,8 +1,9 @@
 import js from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ["dist/"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
